2f45743aae2798a18cc98b5f7bbac3a3db7a7bec
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-06 15:27:14 +0100

    vue greeting with logo

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el : '#root',
+    data: {
+        greetings : 'Once a Booleaner, always a Booleaner!',
+        myClass : 'greet',
+        classImg : 'https://www.boolean.careers/images/misc/logo.png',
+        imgClass : 'img-class'
+    }
+});
